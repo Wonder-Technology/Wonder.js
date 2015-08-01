4fa9ca63ac08ec9be1ad16484a9e73a73f082b74
yyc <395976266@qq.com>
2015-08-01 18:49:19 +0800

    add Component.ts

---
+++ src/core/Component.ts
@@ -0,0 +1,23 @@
+/// <reference path="../definitions.d.ts"/>
+module dy{
+    export class Component{
+        private _gameObject:GameObject = null;
+        get gameObject(){
+            return this._gameObject;
+        }
+        set gameObject(gameObject:GameObject){
+            this._gameObject = gameObject;
+        }
+
+        public get transform():Transform {
+            if(!this._gameObject) {
+                return null;
+            }
+
+            return this._gameObject.transform;
+        }
+
+        public init(){
+        }
+    }
+}
