dfab8975ebaf84e8f219714349c3bbb8afed5bc0
yyc <395976266@qq.com>
2015-10-08 09:29:41 +0800

    modify shadowMap_fragment.glsl->shadowBias: directly return "0.0001" if not pass u_shadowBias

--- src/renderer/shader/chunk/glsl/light/shadowMap_fragment.glsl
+++ src/renderer/shader/chunk/glsl/light/shadowMap_fragment.glsl
@@ -8,11 +8,15 @@ float getShadowBias(vec3 lightDir){
         return u_shadowBias;
     }
 
-    //todo need verify
     /*!
+    //todo need verify
+
      A shadow bias of 0.005 solves the issues of our scene by a large extent, but some surfaces that have a steep angle to the light source might still produce shadow acne. A more solid approach would be to change the amount of bias based on the surface angle towards the light: something we can solve with the dot product:
+
+    return max(0.005 * (1.0 - dot(normalize(getNormal()), lightDir)), 0.001);
      */
-    return max(0.05 * (1.0 - dot(getNormal(), lightDir)), 0.005);
+
+     return 0.001;
 }
 
 float unpackDepth(vec4 rgbaDepth) {
