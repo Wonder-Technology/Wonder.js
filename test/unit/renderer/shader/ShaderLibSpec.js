f8a6354318bc9befd34461012efa7c341385157a
yyc <395976266@qq.com>
2015-10-18 10:41:59 +0800

    refactor: XXXShaderLib->sendXXXData can pass no type now add ShaderLib->sendUniformData, get type from VariableLibType add ShaderLib->sendAttributeData, type is VariableType.Buffer correspond modify XXXShaderLib

---
+++ test/unit/renderer/shader/ShaderLibSpec.js
@@ -0,0 +1,32 @@
+describe("ShaderLib", function () {
+    var sandbox = null;
+    var Lib = null;
+    var lib = null;
+
+    beforeEach(function () {
+        sandbox = sinon.sandbox.create();
+        Lib = dy.ShaderLib;
+        lib = new Lib();
+    });
+    afterEach(function () {
+        sandbox.restore();
+    });
+
+    describe("sendUniformData", function(){
+        var program;
+
+        beforeEach(function(){
+            program = {
+                sendUniformData:sandbox.stub()
+            }
+        });
+
+        it("if uniform name not exist in VariableLib, contract error", function(){
+            testTool.openContractCheck(sandbox);
+
+            expect(function(){
+                lib.sendUniformData(program, "aaa", {});
+            }).toThrow();
+        });
+    });
+});
