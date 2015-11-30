46b41906672486d0f2910dc2f554f299c41c4734
yyc <395976266@qq.com>
2015-11-30 10:02:24 +0800

    add "script" sample

---
+++ examples/samples/script/camera.ts
@@ -0,0 +1,19 @@
+/// <reference path="../../../dist/dy.d.ts"/>
+module sample{
+    @dy.script("camera")
+    export class Camera implements dy.IScriptBehavior{
+        constructor(gameObject:dy.GameObject){
+            this._gameObject = gameObject;
+        }
+
+        private _gameObject:dy.GameObject = null;
+
+        public init() {
+            var scene = dy.Director.getInstance().scene.script.getChild("scene"),
+                cameraComponent = <dy.PerspectiveCamera>this._gameObject.getComponent<dy.CameraController>(dy.CameraController).camera;
+
+            alert(`sceneScript.state is ${scene.state}`);
+            alert(`camera->fovy is ${cameraComponent.fovy}`);
+        }
+    }
+}
