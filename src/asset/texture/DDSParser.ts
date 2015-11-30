9017d96d35ec83cf85c2c6a6b482fc301890def3
yyc <395976266@qq.com>
2015-11-30 16:39:01 +0800

    refactor DDSParser

--- src/asset/texture/DDSParser.ts
+++ src/asset/texture/DDSParser.ts
@@ -1,32 +1,37 @@
 /// <reference path="../../filePath.d.ts"/>
 
-/*! referenced from:
-https://github.com/mrdoob/three.js
-*/
+/*!
+ referenced from:
+ https://github.com/mrdoob/three.js
+ */
 module wd{
     declare var Math:any;
 
-    const DDS_MAGIC = 0x20534444;
-    const DDSD_CAPS = 0x1,
+    const DDS_MAGIC = 0x20534444,
+
+        DDSD_CAPS = 0x1,
         DDSD_HEIGHT = 0x2,
         DDSD_WIDTH = 0x4,
         DDSD_PITCH = 0x8,
         DDSD_PIXELFORMAT = 0x1000,
         DDSD_MIPMAPCOUNT = 0x20000,
         DDSD_LINEARSIZE = 0x80000,
-        DDSD_DEPTH = 0x800000;
-    const DDSCAPS_COMPLEX = 0x8,
+        DDSD_DEPTH = 0x800000,
+
+        DDSCAPS_COMPLEX = 0x8,
         DDSCAPS_MIPMAP = 0x400000,
-        DDSCAPS_TEXTURE = 0x1000;
-    const DDSCAPS2_CUBEMAP = 0x200,
+        DDSCAPS_TEXTURE = 0x1000,
+
+        DDSCAPS2_CUBEMAP = 0x200,
         DDSCAPS2_CUBEMAP_POSITIVEX = 0x400,
         DDSCAPS2_CUBEMAP_NEGATIVEX = 0x800,
         DDSCAPS2_CUBEMAP_POSITIVEY = 0x1000,
         DDSCAPS2_CUBEMAP_NEGATIVEY = 0x2000,
         DDSCAPS2_CUBEMAP_POSITIVEZ = 0x4000,
         DDSCAPS2_CUBEMAP_NEGATIVEZ = 0x8000,
-        DDSCAPS2_VOLUME = 0x200000;
-    const DDPF_ALPHAPIXELS = 0x1,
+        DDSCAPS2_VOLUME = 0x200000,
+
+        DDPF_ALPHAPIXELS = 0x1,
         DDPF_ALPHA = 0x2,
         DDPF_FOURCC = 0x4,
         DDPF_RGB = 0x40,
@@ -35,41 +40,46 @@ module wd{
 
     export class DDSParser{
         public static parse(buffer:any, loadMipmaps:boolean=true){
-            var dds = new DDSData();
+            var dds = new DDSData(),
+                FOURCC_DXT1 = this._fourCCToInt32("DXT1"),
+                FOURCC_DXT3 = this._fourCCToInt32("DXT3"),
+                FOURCC_DXT5 = this._fourCCToInt32("DXT5"),
 
-            var FOURCC_DXT1 = this._fourCCToInt32("DXT1");
-            var FOURCC_DXT3 = this._fourCCToInt32("DXT3");
-            var FOURCC_DXT5 = this._fourCCToInt32("DXT5");
-
-            var headerLengthInt = 31; // The header length in 32 bit ints
+                headerLengthInt = 31, // The header length in 32 bit int,
 
             // Offsets into the header array
 
-            var off_magic = 0;
+                off_magic = 0,
 
-            var off_size = 1;
-            var off_flags = 2;
-            var off_height = 3;
-            var off_width = 4;
+                off_size = 1,
+                off_flags = 2,
+                off_height = 3,
+                off_width = 4,
 
-            var off_mipmapCount = 7;
+                off_mipmapCount = 7,
 
-            var off_pfFlags = 20;
-            var off_pfFourCC = 21;
-            var off_RGBBitCount = 22;
-            var off_RBitMask = 23;
-            var off_GBitMask = 24;
-            var off_BBitMask = 25;
-            var off_ABitMask = 26;
+                off_pfFlags = 20,
+                off_pfFourCC = 21,
+                off_RGBBitCount = 22,
+                off_RBitMask = 23,
+                off_GBitMask = 24,
+                off_BBitMask = 25,
+                off_ABitMask = 26,
 
-            var off_caps = 27;
-            var off_caps2 = 28;
-            var off_caps3 = 29;
-            var off_caps4 = 30;
-
+                off_caps = 27,
+                off_caps2 = 28,
+                off_caps3 = 29,
+                off_caps4 = 30,
             // Parse header
+                header:Int32Array = new Int32Array( buffer, 0, headerLengthInt ),
+                blockBytes = null,
+                fourCC = null,
+                isRGBAUncompressed = null,
+                dataOffset = null,
+                width = null,
+                height = null,
+                faces = null;
 
-            var header:Int32Array = new Int32Array( buffer, 0, headerLengthInt );
 
             if ( header[ off_magic ] !== DDS_MAGIC ) {
                 Log.error(true, "Invalid magic number in DDS header." );
@@ -81,11 +91,10 @@ module wd{
                 return dds;
             }
 
-            var blockBytes;
 
-            var fourCC = header[ off_pfFourCC ];
+            fourCC = header[ off_pfFourCC ];
 
-            var isRGBAUncompressed = false;
+            isRGBAUncompressed = false;
 
             switch ( fourCC ) {
                 case FOURCC_DXT1:
@@ -129,36 +138,38 @@ module wd{
             dds.width = header[ off_width ];
             dds.height = header[ off_height ];
 
-            var dataOffset = header[ off_size ] + 4;
+            dataOffset = header[ off_size ] + 4;
 
             // Extract mipmaps buffers
 
-            var width = dds.width;
-            var height = dds.height;
+            width = dds.width;
+            height = dds.height;
 
-            var faces = dds.isCubemap ? 6 : 1;
+            faces = dds.isCubemap ? 6 : 1;
 
-            for ( var face = 0; face < faces; face ++ ) {
+            for ( let face = 0; face < faces; face ++ ) {
 
-                for ( var i = 0; i < dds.mipmapCount; i ++ ) {
+                for ( let i = 0; i < dds.mipmapCount; i ++ ) {
+                    let mipmap = null,
+                        byteArray = null,
+                        dataLength = null;
 
                     if ( isRGBAUncompressed ) {
-                        var byteArray = this._loadARGBMip( buffer, dataOffset, width, height );
-                        var dataLength = byteArray.length;
+                        byteArray = this._loadARGBMip( buffer, dataOffset, width, height );
+                        dataLength = byteArray.length;
                     }
                     else {
-                        var dataLength = Math.max( 4, width ) / 4 * Math.max( 4, height ) / 4 * blockBytes;
-                        var byteArray = new Uint8Array( buffer, dataOffset, dataLength );
+                        dataLength = Math.max( 4, width ) / 4 * Math.max( 4, height ) / 4 * blockBytes;
+                        byteArray = new Uint8Array( buffer, dataOffset, dataLength );
                     }
 
-                    var mipmap = <CompressedTextureMipmap>{ "data": byteArray, "width": width, "height": height };
+                    mipmap = <CompressedTextureMipmap>{ "data": byteArray, "width": width, "height": height };
                     dds.mipmaps.addChild( mipmap );
 
                     dataOffset += dataLength;
 
                     width = Math.max( width * 0.5, 1 );
                     height = Math.max( height * 0.5, 1 );
-
                 }
 
                 width = dds.width;
@@ -185,23 +196,35 @@ module wd{
             );
         }
         private static _loadARGBMip( buffer, dataOffset, width, height ) {
-            var dataLength = width * height * 4;
-            var srcBuffer = new Uint8Array( buffer, dataOffset, dataLength );
-            var byteArray = new Uint8Array( dataLength );
-            var dst = 0;
-            var src = 0;
-            for ( var y = 0; y < height; y ++ ) {
-                for ( var x = 0; x < width; x ++ ) {
-                    var b = srcBuffer[src]; src ++;
-                    var g = srcBuffer[src]; src ++;
-                    var r = srcBuffer[src]; src ++;
-                    var a = srcBuffer[src]; src ++;
+            var dataLength = width * height * 4,
+                srcBuffer = new Uint8Array( buffer, dataOffset, dataLength ),
+                byteArray = new Uint8Array( dataLength ),
+                dst = 0,
+                src = 0;
+
+            for ( let y = 0; y < height; y ++ ) {
+                for ( let x = 0; x < width; x ++ ) {
+                    let b = null,
+                        g = null,
+                        r = null,
+                        a = null;
+
+                    b = srcBuffer[src];
+                    src ++;
+                    g = srcBuffer[src];
+                    src ++;
+                    r = srcBuffer[src];
+                    src ++;
+                    a = srcBuffer[src];
+                    src ++;
+
                     byteArray[dst] = r; dst ++;	//r
                     byteArray[dst] = g; dst ++;	//g
                     byteArray[dst] = b; dst ++;	//b
                     byteArray[dst] = a; dst ++;	//a
                 }
             }
+
             return byteArray;
         }
     }
