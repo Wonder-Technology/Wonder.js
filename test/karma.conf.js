c1301b71a137b7c3241b1ae33953ef6fc17273d9
yyc <395976266@qq.com>
2015-12-02 10:19:10 +0800

    refactor: clean unit test

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -101,7 +101,7 @@
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome', 'Firefox'],
+    browsers: ['Chrome'],
 
 
     // Continuous Integration mode
