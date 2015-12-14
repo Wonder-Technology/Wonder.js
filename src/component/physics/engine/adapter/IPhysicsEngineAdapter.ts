15ad518901d042f362554c28918ae43a04e07aec
yyc <395976266@qq.com>
2015-12-14 20:53:56 +0800

    refactor IPhysicsEngineAdapter:add "setXXX","getXXX" method definition

--- src/component/physics/engine/adapter/IPhysicsEngineAdapter.ts
+++ src/component/physics/engine/adapter/IPhysicsEngineAdapter.ts
@@ -5,6 +5,24 @@ module wd {
 
         init():void;
         update(time:number):void;
+
+        getGravity(gravity:number):Vector3;
+        setGravity( gravity:Vector3):void;
+        getFriction(obj:GameObject, friction:number):number;
+        setFriction(obj:GameObject, friction:number):void;
+        getRestitution(obj:GameObject, restitution:number):number;
+        setRestitution(obj:GameObject, restitution:number):void;
+        getLinearDamping(obj:GameObject):number;
+        setLinearDamping(obj:GameObject, linearDamping:number):void;
+        getAngularDamping(obj:GameObject):number;
+        setAngularDamping(obj:GameObject, angularDamping:number):void;
+        getMass(obj:GameObject):number;
+        setMass(obj:GameObject, mass:number):void;
+        getVelocity(obj:GameObject):Vector3;
+        setVelocity(obj:GameObject, velocity:Vector3):void;
+        getAngularVelocity(obj:GameObject):Vector3;
+        setAngularVelocity(obj:GameObject, angularVelocity:Vector3):void;
+
         addDynamicBody(gameObject:GameObject, shape:Shape, options:any):void;
         addKinematicBody(gameObject:GameObject, shape:Shape, options:any):void;
         addStaticBody(gameObject:GameObject, shape:Shape, options:any):void;
