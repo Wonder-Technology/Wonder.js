d82482fedf4c102619568a9eac6d1fc4d42c34b3
yyc <395976266@qq.com>
2016-02-22 20:37:17 +0800

    rename benchmark "benchmark_1000_boxes" to "benchmark_1000_baic_boxes"
    add ETransformState

---
+++ src/component/transform/ETransformState.ts
@@ -0,0 +1,11 @@
+module wd{
+    export enum ETransformState{
+        ISTRANSLATE = <any>"isTranslate",
+        ISROTATE = <any>"isRotate",
+        ISSCALE = <any>"isScale",
+
+        ISLOCALTRANSLATE = <any>"isLocalTranslate",
+        ISLOCALROTATE = <any>"isLocalRotate",
+        ISLOCALSCALE = <any>"isLocalScale"
+    }
+}
