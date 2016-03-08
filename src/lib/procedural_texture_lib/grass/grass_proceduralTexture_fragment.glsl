9bf9577e49bf019997a7f05dc8660309a76f8a76
yyc <395976266@qq.com>
2016-03-08 10:31:53 +0800

    modify grass_proceduralTexture_fragment.glsl

--- src/lib/procedural_texture_lib/grass/grass_proceduralTexture_fragment.glsl
+++ src/lib/procedural_texture_lib/grass/grass_proceduralTexture_fragment.glsl
@@ -8,7 +8,7 @@ uniform vec3 u_groundColor;
 @end
 
 @body
-	vec3 color = mix(u_groundColor, u_herb1Color, rand(v_texCoord.xy * 4.0));
+	vec3 color = mix(u_groundColor, u_herb1Color, rand(gl_FragCoord.xy * 4.0));
 	color = mix(color, u_herb2Color, rand(gl_FragCoord.xy * 8.0));
 	color = mix(color, u_herb3Color, rand(gl_FragCoord.xy));
 	color = mix(color, u_herb1Color, fbm(gl_FragCoord.xy * 16.0));
