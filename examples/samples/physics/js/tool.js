1268ba98251363c99f5003d6b068cb34539d9512
yyc <395976266@qq.com>
2016-04-10 14:20:26 +0800

    pass all samples: physics_compound show shadow

--- examples/samples/physics/js/tool.js
+++ examples/samples/physics/js/tool.js
@@ -8,7 +8,7 @@ var tool = {
 
         return ambientLight;
     },
-    createDirectionLight: function (shadowList) {
+    createDirectionLight: function () {
         var SHADOW_MAP_WIDTH = 1024,
             SHADOW_MAP_HEIGHT = 1024;
 
@@ -16,21 +16,20 @@ var tool = {
         directionLightComponent.color = wd.Color.create("#ffffff");
         directionLightComponent.intensity = 1;
 
-        if(shadowList){
-            directionLightComponent.castShadow = true;
-            directionLightComponent.shadowCameraLeft = -100;
-            directionLightComponent.shadowCameraRight = 100;
-            directionLightComponent.shadowCameraTop = 100;
-            directionLightComponent.shadowCameraBottom = -100;
-            directionLightComponent.shadowCameraNear = 0.1;
-            directionLightComponent.shadowCameraFar = 1000;
-            directionLightComponent.shadowBias = 0.002;
-            directionLightComponent.shadowDarkness = 0.2;
-            directionLightComponent.shadowMapWidth = SHADOW_MAP_WIDTH;
-            directionLightComponent.shadowMapHeight = SHADOW_MAP_HEIGHT;
+        //if(shadowList){
+        directionLightComponent.castShadow = true;
+        directionLightComponent.shadowCameraLeft = -100;
+        directionLightComponent.shadowCameraRight = 100;
+        directionLightComponent.shadowCameraTop = 100;
+        directionLightComponent.shadowCameraBottom = -100;
+        directionLightComponent.shadowCameraNear = 0.1;
+        directionLightComponent.shadowCameraFar = 1000;
+        directionLightComponent.shadowBias = 0.002;
+        directionLightComponent.shadowDarkness = 0.2;
+        directionLightComponent.shadowMapWidth = SHADOW_MAP_WIDTH;
+        directionLightComponent.shadowMapHeight = SHADOW_MAP_HEIGHT;
 
-            directionLightComponent.shadowRenderList = shadowList;
-        }
+        //}
 
 
         var directionLight = wd.GameObject.create();
@@ -41,34 +40,40 @@ var tool = {
         return directionLight;
     },
     createGround: function () {
-            var material = wd.LightMaterial.create();
-            material.color = wd.Color.create("#dddddd");
+        var material = wd.LightMaterial.create();
+        material.color = wd.Color.create("#dddddd");
 
-            var geometry = wd.BoxGeometry.create();
-            geometry.material = material;
-            geometry.width = 100;
-            geometry.height = 1;
-            geometry.depth = 100;
-
-
-            var collision = wd.BoxCollider.create();
+        var geometry = wd.BoxGeometry.create();
+        geometry.material = material;
+        geometry.width = 100;
+        geometry.height = 1;
+        geometry.depth = 100;
 
 
-            var rigidBody = wd.StaticRigidBody.create();
+        var collision = wd.BoxCollider.create();
 
 
-            var gameObject = wd.GameObject.create();
-            gameObject.addComponent(geometry);
-            gameObject.addComponent(collision);
-            gameObject.addComponent(rigidBody);
-
-            gameObject.addComponent(wd.MeshRenderer.create());
+        var rigidBody = wd.StaticRigidBody.create();
 
 
-            gameObject.transform.translate(0, -2.5, 0);
+        var gameObject = wd.GameObject.create();
+        gameObject.addComponent(geometry);
+        gameObject.addComponent(collision);
+        gameObject.addComponent(rigidBody);
+
+        gameObject.addComponent(wd.MeshRenderer.create());
+
+        var shadow = wd.Shadow.create();
+        shadow.cast = false;
+        shadow.receive = true;
+
+        gameObject.addComponent(shadow);
 
 
-            return gameObject;
+        gameObject.transform.translate(0, -2.5, 0);
+
+
+        return gameObject;
     },
     createCamera: function (distance,target) {
             var camera = wd.GameObject.create(),
