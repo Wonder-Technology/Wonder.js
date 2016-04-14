3d33fb46acb723fee94ae1085ddaf60bbf9461d1
yyc <395976266@qq.com>
2016-04-14 08:26:21 +0800

    move shadow unit test to renderer/shadow/
    pass all unit test

--- test/unit/component/shadow/ShadowSpec.js
+++ test/unit/component/shadow/ShadowSpec.js
@@ -20,13 +20,13 @@ describe("Shadow", function() {
             shadow.entityObject = wd.GameObject.create();
             shadow.receive = true;
             shadow.init();
-            sandbox.stub(shadow, "_handleReceive");
+            sandbox.stub(shadow, "_addAllShadowMapsToObjectAndChildren");
 
             shadow.dispose();
 
             wd.EventManager.trigger(wd.CustomEvent.create(wd.EEngineEvent.SHADOWMAP_LAYER_CHANGE));
 
-            expect(shadow._handleReceive).not.toCalled();
+            expect(shadow._addAllShadowMapsToObjectAndChildren).not.toCalled();
         });
     });
 });
