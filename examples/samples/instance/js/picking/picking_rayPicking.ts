9d2a01936d5c018dabc466d310cf35b5e7f4bcfc
yyc <395976266@qq.com>
2016-04-22 09:45:31 +0800

    add "instance_rayPicking_perspective" sample

---
+++ examples/samples/instance/js/picking/picking_rayPicking.ts
@@ -0,0 +1,46 @@
+/// <reference path="../../../../../dist/wd.d.ts"/>
+module sample {
+    import GameObject = wd.GameObject;
+    import MouseEvent = wd.MouseEvent;
+
+    @wd.script("RayPicking")
+    export class RayPicking implements wd.IScriptBehavior {
+        constructor(gameObject:GameObject) {
+            this._gameObject = gameObject;
+        }
+
+        private _gameObject:GameObject = null;
+
+        public onMouseDown(e:MouseEvent){
+            this._handleSelect(this._gameObject);
+        }
+
+        private _handleSelect(selectedObj:GameObject) {
+            var tween1 = wd.Tween.create(),
+                tween2 = wd.Tween.create(),
+                action = null;
+
+            console.log(`select ${selectedObj.uid}`);
+
+            tween1.from({x: 1})
+                .to({x: 2}, 300)
+                .easing(wd.Tween.Easing.Cubic.InOut)
+                .onUpdate(function () {
+                    selectedObj.transform.scale = wd.Vector3.create(this.x, this.x, this.x);
+                });
+
+            tween2.from({x: 2})
+                .to({x: 1}, 300)
+                .easing(wd.Tween.Easing.Cubic.InOut)
+                .onUpdate(function () {
+                    selectedObj.transform.scale = wd.Vector3.create(this.x, this.x, this.x);
+                });
+
+            action = wd.Repeat.create(wd.Sequence.create(tween1, tween2), 2);
+
+            selectedObj.addComponent(action);
+
+            action.init();
+        }
+    }
+}
