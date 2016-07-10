1c022fa2df1f64a3f4f3a4c6fcabf2c592ac1276
yyc <395976266@qq.com>
2016-07-11 05:41:15 +0800

    fix alpha problem: uiThreeD_font_bitmap sample->set material.blendType to NORMAL modify map_forBasic_fragment.glsl corresponding modify multi_map_forBasic_fragment.glsl

--- src/renderer/shader/chunk/glsl/map/map_forBasic_fragment.glsl
+++ src/renderer/shader/chunk/glsl/map/map_forBasic_fragment.glsl
@@ -3,5 +3,6 @@ varying vec2 v_mapCoord0;
 @end
 
 @body
-    totalColor *= vec4(texture2D(u_sampler2D0, v_mapCoord0).xyz, 1.0);
+//todo test
+    totalColor *= texture2D(u_sampler2D0, v_mapCoord0);
 @end
