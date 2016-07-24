fd052a52d12812c640360d9a456412a1b983fdca
yyc <395976266@qq.com>
2016-07-24 20:28:06 +0800

    render test: move texture_reflection sample to render scene test

--- test/render/test/texture/textureSpec.js
+++ test/render/test/texture/textureSpec.js
@@ -263,5 +263,157 @@ describe("texture", function () {
                 tester.compareAt(1, "texture/texture_compressed.png", done);
             });
         });
+
+        describe("test reflection texture", function () {
+            var tester;
+
+            function body(assetParentDirPath, done){
+
+        wd.LoaderManager.getInstance().load([
+            {url: assetParentDirPath + "asset/texture/1.jpg", id: "texture"},
+            {url: assetParentDirPath + "asset/texture/skybox/px.jpg", id: "px"},
+            {url: assetParentDirPath + "asset/texture/skybox/nx.jpg", id: "nx"},
+            {url: assetParentDirPath + "asset/texture/skybox/py.jpg", id: "py"},
+            {url: assetParentDirPath + "asset/texture/skybox/ny.jpg", id: "ny"},
+            {url: assetParentDirPath + "asset/texture/skybox/pz.jpg", id: "pz"},
+            {url: assetParentDirPath + "asset/texture/skybox/nz.jpg", id: "nz"}
+        ]).subscribe(null, null, function () {
+            initSample();
+
+
+            tester.init();
+
+            if(done){
+                done();
+            }
+        });
+
+        function initSample() {
+            var director = wd.Director.getInstance();
+
+            director.scene.addChild(createSkybox());
+            director.scene.addChild(createSphere());
+            director.scene.addChild(createCamera());
+
+            //director.start();
+        }
+
+        function createSkybox() {
+            var cubemap = wd.CubemapTexture.create(
+                [
+                    {
+                        asset: wd.LoaderManager.getInstance().get("px")
+                    },
+                    {
+                        asset: wd.LoaderManager.getInstance().get("nx")
+                    },
+                    {
+                        asset: wd.LoaderManager.getInstance().get("py")
+                    },
+                    {
+                        asset: wd.LoaderManager.getInstance().get("ny")
+                    },
+                    {
+                        asset: wd.LoaderManager.getInstance().get("pz")
+                    },
+                    {
+                        asset: wd.LoaderManager.getInstance().get("nz")
+                    }
+                ]
+            );
+
+            var material = wd.SkyboxMaterial.create();
+            material.envMap = cubemap;
+
+
+            var geometry = wd.BoxGeometry.create();
+            geometry.material = material;
+            geometry.width = 5;
+            geometry.height = 5;
+            geometry.depth = 5;
+
+
+            var gameObject = wd.GameObject.create();
+
+            gameObject.addComponent(wd.SkyboxRenderer.create());
+            gameObject.addComponent(geometry);
+
+            return gameObject;
+        }
+
+        function createSphere() {
+            var cubemap = wd.CubemapTexture.create(
+                [
+                    {
+                        asset: wd.LoaderManager.getInstance().get("px")
+                    },
+                    {
+                        asset: wd.LoaderManager.getInstance().get("nx")
+                    },
+                    {
+                        asset: wd.LoaderManager.getInstance().get("py")
+                    },
+                    {
+                        asset: wd.LoaderManager.getInstance().get("ny")
+                    },
+                    {
+                        asset: wd.LoaderManager.getInstance().get("pz")
+                    },
+                    {
+                        asset: wd.LoaderManager.getInstance().get("nz")
+                    }
+                ]
+            );
+            cubemap.mode = wd.EEnvMapMode.REFLECTION;
+
+
+            var material = wd.BasicMaterial.create();
+            material.envMap = cubemap;
+            material.shading = wd.EShading.SMOOTH;
+
+            var geometry = wd.SphereGeometry.create();
+            geometry.material = material;
+            geometry.radius = 5;
+
+            var gameObject = wd.GameObject.create();
+            gameObject.addComponent(geometry);
+
+            gameObject.addComponent(wd.MeshRenderer.create());
+
+            return gameObject;
+        }
+
+
+        function createCamera() {
+            var camera = wd.GameObject.create(),
+                view = wd.Director.getInstance().view,
+                cameraComponent = wd.PerspectiveCamera.create();
+
+            cameraComponent.fovy = 60;
+            cameraComponent.aspect = view.width / view.height;
+            cameraComponent.near = 0.1;
+            cameraComponent.far = 1000;
+
+            var controller = wd.FlyCameraController.create(cameraComponent);
+            camera.addComponent(controller);
+
+            camera.transform.translate(0, 0, 20);
+
+            return camera;
+        }
+            }
+
+            beforeEach(function (done) {
+                tester = SceneTester.create();
+
+                renderTestTool.prepareContext();
+
+                tester.execBody(body, done);
+            });
+
+            it("test", function (done) {
+                tester.compareAt(1, "texture/texture_reflection.png", done);
+            });
+        });
     });
 });
--- test/render/tool/generateCorrectImageToolSpec.js
+++ test/render/tool/generateCorrectImageToolSpec.js
@@ -4,7 +4,13 @@ describe("generate correct image tool", function () {
     function body(assetParentDirPath, done){
 
         wd.LoaderManager.getInstance().load([
-            {url: assetParentDirPath + "asset/texture/compressed/disturb_dxt1_mip.dds", id: "texture"}
+            {url: assetParentDirPath + "asset/texture/1.jpg", id: "texture"},
+            {url: assetParentDirPath + "asset/texture/skybox/px.jpg", id: "px"},
+            {url: assetParentDirPath + "asset/texture/skybox/nx.jpg", id: "nx"},
+            {url: assetParentDirPath + "asset/texture/skybox/py.jpg", id: "py"},
+            {url: assetParentDirPath + "asset/texture/skybox/ny.jpg", id: "ny"},
+            {url: assetParentDirPath + "asset/texture/skybox/pz.jpg", id: "pz"},
+            {url: assetParentDirPath + "asset/texture/skybox/nz.jpg", id: "nz"}
         ]).subscribe(null, null, function () {
             initSample();
 
@@ -19,32 +25,98 @@ describe("generate correct image tool", function () {
         function initSample() {
             var director = wd.Director.getInstance();
 
-            director.scene.addChild(createTriangle());
+            director.scene.addChild(createSkybox());
+            director.scene.addChild(createSphere());
             director.scene.addChild(createCamera());
 
             //director.start();
         }
 
-        function createTriangle() {
-            var material = wd.BasicMaterial.create();
+        function createSkybox() {
+            var cubemap = wd.CubemapTexture.create(
+                [
+                    {
+                        asset: wd.LoaderManager.getInstance().get("px")
+                    },
+                    {
+                        asset: wd.LoaderManager.getInstance().get("nx")
+                    },
+                    {
+                        asset: wd.LoaderManager.getInstance().get("py")
+                    },
+                    {
+                        asset: wd.LoaderManager.getInstance().get("ny")
+                    },
+                    {
+                        asset: wd.LoaderManager.getInstance().get("pz")
+                    },
+                    {
+                        asset: wd.LoaderManager.getInstance().get("nz")
+                    }
+                ]
+            );
 
-            material.map = wd.LoaderManager.getInstance().get("texture").toTexture();
+            var material = wd.SkyboxMaterial.create();
+            material.envMap = cubemap;
 
 
-            var geometry = wd.RectGeometry.create();
+            var geometry = wd.BoxGeometry.create();
             geometry.material = material;
             geometry.width = 5;
             geometry.height = 5;
+            geometry.depth = 5;
 
+
+            var gameObject = wd.GameObject.create();
+
+            gameObject.addComponent(wd.SkyboxRenderer.create());
+            gameObject.addComponent(geometry);
+
+            return gameObject;
+        }
+
+        function createSphere() {
+            var cubemap = wd.CubemapTexture.create(
+                [
+                    {
+                        asset: wd.LoaderManager.getInstance().get("px")
+                    },
+                    {
+                        asset: wd.LoaderManager.getInstance().get("nx")
+                    },
+                    {
+                        asset: wd.LoaderManager.getInstance().get("py")
+                    },
+                    {
+                        asset: wd.LoaderManager.getInstance().get("ny")
+                    },
+                    {
+                        asset: wd.LoaderManager.getInstance().get("pz")
+                    },
+                    {
+                        asset: wd.LoaderManager.getInstance().get("nz")
+                    }
+                ]
+            );
+            cubemap.mode = wd.EEnvMapMode.REFLECTION;
+
+
+            var material = wd.BasicMaterial.create();
+            material.envMap = cubemap;
+            material.shading = wd.EShading.SMOOTH;
+
+            var geometry = wd.SphereGeometry.create();
+            geometry.material = material;
+            geometry.radius = 5;
 
             var gameObject = wd.GameObject.create();
             gameObject.addComponent(geometry);
 
             gameObject.addComponent(wd.MeshRenderer.create());
 
-
             return gameObject;
         }
+
 
         function createCamera() {
             var camera = wd.GameObject.create(),
@@ -54,15 +126,16 @@ describe("generate correct image tool", function () {
             cameraComponent.fovy = 60;
             cameraComponent.aspect = view.width / view.height;
             cameraComponent.near = 0.1;
-            cameraComponent.far = 80;
+            cameraComponent.far = 1000;
 
-            var controller = wd.BasicCameraController.create(cameraComponent);
+            var controller = wd.FlyCameraController.create(cameraComponent);
             camera.addComponent(controller);
 
-            camera.transform.translate(0, 0, 5);
+            camera.transform.translate(0, 0, 20);
 
             return camera;
         }
+
 
     }
 
@@ -81,7 +154,7 @@ describe("generate correct image tool", function () {
             [
                 {
                     frameIndex:1,
-                    imageName:"texture_compressed.png"
+                    imageName:"texture_reflection.png"
                 }
             ]
         );
