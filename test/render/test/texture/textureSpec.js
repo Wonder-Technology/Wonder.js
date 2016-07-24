788a47731534a8841fab6fc4628d8cc2077920af
yyc <395976266@qq.com>
2016-07-24 20:19:27 +0800

    render test: move texture_anisotropic sample to render scene test

---
+++ test/render/test/texture/textureSpec.js
@@ -0,0 +1,103 @@
+describe("texture", function () {
+    var sandbox = null;
+
+    beforeEach(function () {
+        sandbox = sinon.sandbox.create();
+    });
+    afterEach(function () {
+        testTool.clearInstance(sandbox);
+
+        renderTestTool.destoryContext();
+
+        sandbox.restore();
+    });
+
+    describe("scene test", function() {
+        describe("test anisotropic", function () {
+            var tester;
+
+            function body(assetParentDirPath, done){
+
+                wd.LoaderManager.getInstance().load([
+                    {url: assetParentDirPath + "asset/texture/crate.gif", id: "texture"}
+                ]).subscribe(null, null, function () {
+                    initSample();
+
+
+                    tester.init();
+
+                    if(done){
+                        done();
+                    }
+                });
+
+                function initSample() {
+                    var director = wd.Director.getInstance();
+
+                    director.scene.addChild(createPlane());
+                    director.scene.addChild(createCamera());
+
+                    //director.start();
+                }
+
+                function createPlane() {
+                    var map = wd.LoaderManager.getInstance().get("texture").toTexture();
+                    map.wrapS = map.wrapT = wd.ETextureWrapMode.REPEAT;
+                    map.repeatRegion = wd.RectRegion.create(0, 0, 512, 512);
+                    /*!annotate this line to see the difference*/
+                    map.anisotropy = wd.GPUDetector.getInstance().maxAnisotropy;
+
+                    var material = wd.BasicMaterial.create();
+                    material.map = map;
+
+
+                    var geometry = wd.PlaneGeometry.create();
+                    geometry.material = material;
+                    geometry.width = 100;
+                    geometry.height = 100;
+
+                    var gameObject = wd.GameObject.create();
+                    gameObject.addComponent(geometry);
+
+                    gameObject.addComponent(wd.MeshRenderer.create());
+
+                    gameObject.transform.rotate(wd.Vector3.create(30, 0, 0));
+                    gameObject.transform.scale = wd.Vector3.create(100, 100, 100);
+
+                    return gameObject;
+                }
+
+                function createCamera() {
+                    var camera = wd.GameObject.create(),
+                        view = wd.Director.getInstance().view,
+                        cameraComponent = wd.PerspectiveCamera.create();
+
+                    cameraComponent.fovy = 60;
+                    cameraComponent.aspect = view.width / view.height;
+                    cameraComponent.near = 0.1;
+                    cameraComponent.far = 1000;
+
+                    var controller = wd.FlyCameraController.create(cameraComponent);
+                    camera.addComponent(controller);
+
+                    camera.transform.translate(wd.Vector3.create(0, 0, 100));
+
+                    return camera;
+                }
+
+            }
+
+            beforeEach(function (done) {
+                tester = SceneTester.create();
+
+                renderTestTool.prepareContext();
+
+                tester.execBody(body, done);
+            });
+
+            it("test", function (done) {
+                tester.compareAt(1, "texture/texture_anisotropic.png", done);
+            });
+        });
+    });
+});
--- test/render/tool/generateCorrectImageToolSpec.js
+++ test/render/tool/generateCorrectImageToolSpec.js
@@ -2,16 +2,9 @@ describe("generate correct image tool", function () {
     var tester;
 
     function body(assetParentDirPath, done){
+
         wd.LoaderManager.getInstance().load([
-            {url: assetParentDirPath + "asset/texture/water/bump.png", id: "bump"},
-            {url: assetParentDirPath + "asset/texture/terrain/ground.jpg", id: "ground"},
-            {url: assetParentDirPath + "asset/texture/1.jpg", id: "sphere"},
-            {url: assetParentDirPath + "asset/texture/skybox/px.jpg", id: "px"},
-            {url: assetParentDirPath + "asset/texture/skybox/nx.jpg", id: "nx"},
-            {url: assetParentDirPath + "asset/texture/skybox/py.jpg", id: "py"},
-            {url: assetParentDirPath + "asset/texture/skybox/ny.jpg", id: "ny"},
-            {url: assetParentDirPath + "asset/texture/skybox/pz.jpg", id: "pz"},
-            {url: assetParentDirPath + "asset/texture/skybox/nz.jpg", id: "nz"}
+            {url: assetParentDirPath + "asset/texture/crate.gif", id: "texture"}
         ]).subscribe(null, null, function () {
             initSample();
 
@@ -26,179 +19,37 @@ describe("generate correct image tool", function () {
         function initSample() {
             var director = wd.Director.getInstance();
 
-            var skybox = createSkybox();
-            var sphere = createSphere();
-            var ground = createGround();
-
-            var water = createWater(skybox, sphere, ground);
-            director.scene.addChild(skybox);
-            director.scene.addChild(sphere);
-            director.scene.addChild(ground);
-            director.scene.addChild(water);
-
-            director.scene.addChild(createAmbientLight());
-            director.scene.addChild(createDirectionLight());
-
+            director.scene.addChild(createPlane());
             director.scene.addChild(createCamera());
 
             //director.start();
-
-
-
-            var renderList = [skybox, sphere, ground];
-
-            wd.Director.getInstance().scheduler.scheduleFrame(function(){
-                water.getComponent(wd.Geometry).material.reflectionMap.renderList = [];
-                water.getComponent(wd.Geometry).material.refractionMap.renderList = [];
-            }, 1);
-
-            wd.Director.getInstance().scheduler.scheduleFrame(function(){
-                water.getComponent(wd.Geometry).material.reflectionMap.renderList = renderList;
-                water.getComponent(wd.Geometry).material.refractionMap.renderList = renderList;
-            }, 2);
         }
 
-        function createSkybox() {
-            var cubemap = wd.CubemapTexture.create(
-                [
-                    {
-                        asset: wd.LoaderManager.getInstance().get("px")
-                    },
-                    {
-                        asset: wd.LoaderManager.getInstance().get("nx")
-                    },
-                    {
-                        asset: wd.LoaderManager.getInstance().get("py")
-                    },
-                    {
-                        asset: wd.LoaderManager.getInstance().get("ny")
-                    },
-                    {
-                        asset: wd.LoaderManager.getInstance().get("pz")
-                    },
-                    {
-                        asset: wd.LoaderManager.getInstance().get("nz")
-                    }
-                ]
-            );
+        function createPlane() {
+            var map = wd.LoaderManager.getInstance().get("texture").toTexture();
+            map.wrapS = map.wrapT = wd.ETextureWrapMode.REPEAT;
+            map.repeatRegion = wd.RectRegion.create(0, 0, 512, 512);
+            /*!annotate this line to see the difference*/
+            map.anisotropy = wd.GPUDetector.getInstance().maxAnisotropy;
 
-            var material = wd.SkyboxMaterial.create();
-            material.envMap = cubemap;
-
-
-            var geometry = wd.BoxGeometry.create();
-            geometry.material = material;
-            geometry.width = 5;
-            geometry.height = 5;
-            geometry.depth = 5;
-
-
-            var gameObject = wd.GameObject.create();
-
-            gameObject.addComponent(wd.SkyboxRenderer.create());
-            gameObject.addComponent(geometry);
-
-            return gameObject;
-        }
-
-        function createSphere() {
-            var material = wd.LightMaterial.create();
-            material.diffuseMap = wd.LoaderManager.getInstance().get("sphere").toTexture();
-
-            var geometry = wd.SphereGeometry.create();
-            geometry.material = material;
-            geometry.radius = 5;
-
-            var gameObject = wd.GameObject.create();
-            gameObject.addComponent(geometry);
-
-            gameObject.addComponent(wd.MeshRenderer.create());
-
-            gameObject.transform.translate(0, 20, 0);
-
-            return gameObject;
-        }
-
-        function createWater(skybox, sphere, ground) {
-            var material = wd.WaterMaterial.create();
-            material.color = wd.Color.create("rgb(0.1, 0.2, 0.1)");
-            material.fresnelLevel = 0.5;
-            material.refractionLevel = 0.8;
-            material.reflectionLevel = 0.8;
-
-            material.bumpMap = wd.LoaderManager.getInstance().get("bump").toTexture();
-
-            var reflectionTexture = wd.MirrorTexture.create();
-            reflectionTexture.width = 512;
-            reflectionTexture.height = 512;
-            reflectionTexture.renderList = [skybox, sphere, ground];
-
-            var refractionTexture = wd.RefractionTexture.create();
-            refractionTexture.width = 512;
-            refractionTexture.height = 512;
-            refractionTexture.renderList = [skybox, sphere, ground];
-
-            material.reflectionMap = reflectionTexture;
-            material.refractionMap = refractionTexture;
-
+            var material = wd.BasicMaterial.create();
+            material.map = map;
 
 
             var geometry = wd.PlaneGeometry.create();
             geometry.material = material;
-            geometry.width = 50;
-            geometry.height = 50;
+            geometry.width = 100;
+            geometry.height = 100;
 
             var gameObject = wd.GameObject.create();
             gameObject.addComponent(geometry);
 
             gameObject.addComponent(wd.MeshRenderer.create());
 
-            gameObject.transform.translate(0, 10, 0);
+            gameObject.transform.rotate(wd.Vector3.create(30, 0, 0));
+            gameObject.transform.scale = wd.Vector3.create(100, 100, 100);
 
             return gameObject;
-        }
-
-        function createGround() {
-            var material = wd.LightMaterial.create();
-            material.diffuseMap = wd.LoaderManager.getInstance().get("ground").toTexture();
-
-            var geometry = wd.PlaneGeometry.create();
-            geometry.material = material;
-            geometry.width = 50;
-            geometry.height = 50;
-
-            var gameObject = wd.GameObject.create();
-            gameObject.addComponent(geometry);
-
-            gameObject.addComponent(wd.MeshRenderer.create());
-
-            gameObject.transform.translate(0, 0, 0);
-
-            return gameObject;
-        }
-
-        function createAmbientLight() {
-            var ambientLightComponent = wd.AmbientLight.create();
-            ambientLightComponent.color = wd.Color.create("rgb(30, 30, 30)");
-
-            var ambientLight = wd.GameObject.create();
-            ambientLight.addComponent(ambientLightComponent);
-
-            return ambientLight;
-        }
-
-        function createDirectionLight() {
-            var directionLightComponent = wd.DirectionLight.create();
-            directionLightComponent.color = wd.Color.create("#ffffff");
-            directionLightComponent.intensity = 1;
-
-
-            var directionLight = wd.GameObject.create();
-            directionLight.addComponent(directionLightComponent);
-
-            directionLight.transform.translate(wd.Vector3.create(100, 100, 100));
-
-            return directionLight;
         }
 
         function createCamera() {
@@ -214,11 +65,11 @@ describe("generate correct image tool", function () {
             var controller = wd.FlyCameraController.create(cameraComponent);
             camera.addComponent(controller);
 
-            camera.transform.translate(0, 20, 50);
-            camera.transform.lookAt(0, 10, 0);
+            camera.transform.translate(wd.Vector3.create(0, 0, 100));
 
             return camera;
         }
+
     }
 
     beforeEach(function (done) {
@@ -236,11 +87,7 @@ describe("generate correct image tool", function () {
             [
                 {
                     frameIndex:1,
-                    imageName:"water_remove_reflectionAndRefraction.png"
-                },
-                {
-                    frameIndex:2,
-                    imageName:"water_add_reflectionAndRefraction.png"
+                    imageName:"texture_anisotropic.png"
                 }
             ]
         );
