c07fd81a7a15460aa3528af885fbda77a1ef9525
yyc <395976266@qq.com>
2016-07-24 21:46:37 +0800

    render test: move texture_multi sample to render scene test

--- test/render/test/texture/textureSpec.js
+++ test/render/test/texture/textureSpec.js
@@ -841,5 +841,84 @@ describe("texture", function () {
                 });
             });
         });
+
+        describe("test multi texture", function () {
+            var tester;
+
+            function body(assetParentDirPath, done){
+
+        wd.LoaderManager.getInstance().load([
+            {url: assetParentDirPath + "asset/texture/1.jpg", id: "texture1"},
+            {url: assetParentDirPath + "asset/texture/2.jpg", id: "texture2"}
+        ]).subscribe(null, null, function () {
+            initSample();
+
+
+            tester.init();
+
+            if(done){
+                done();
+            }
+        });
+
+        function initSample() {
+            var director = wd.Director.getInstance();
+
+            director.scene.addChild(createTriangle());
+            director.scene.addChild(createCamera());
+
+            //director.start();
+        }
+
+        function createTriangle() {
+            var material = wd.BasicMaterial.create();
+            material.map = [wd.LoaderManager.getInstance().get("texture1").toTexture(), wd.LoaderManager.getInstance().get("texture2").toTexture()];
+            material.side = wd.ESide.BOTH;
+
+
+            var geometry = wd.TriangleGeometry.create();
+            geometry.material = material;
+            geometry.width = 5;
+            geometry.height = 5;
+
+
+            var gameObject = wd.GameObject.create();
+            gameObject.addComponent(geometry);
+            gameObject.addComponent(wd.MeshRenderer.create());
+
+            return gameObject;
+        }
+
+        function createCamera() {
+            var camera = wd.GameObject.create(),
+                view = wd.Director.getInstance().view,
+                cameraComponent = wd.PerspectiveCamera.create();
+
+            cameraComponent.fovy = 60;
+            cameraComponent.aspect = view.width / view.height;
+            cameraComponent.near = 0.1;
+            cameraComponent.far = 80;
+
+            var controller = wd.BasicCameraController.create(cameraComponent);
+            camera.addComponent(controller);
+
+            camera.transform.translate(wd.Vector3.create(0, 0, 5));
+
+            return camera;
+        }
+            }
+
+            beforeEach(function (done) {
+                tester = SceneTester.create();
+
+                renderTestTool.prepareContext();
+
+                tester.execBody(body, done);
+            });
+
+            it("test", function (done) {
+                tester.compareAt(1, "texture/texture_multi.png", done);
+            });
+        });
     });
 });
--- test/render/tool/generateCorrectImageToolSpec.js
+++ test/render/tool/generateCorrectImageToolSpec.js
@@ -4,7 +4,8 @@ describe("generate correct image tool", function () {
     function body(assetParentDirPath, done){
 
         wd.LoaderManager.getInstance().load([
-            {url: assetParentDirPath + "asset/texture/1.jpg", id: "texture"}
+            {url: assetParentDirPath + "asset/texture/1.jpg", id: "texture1"},
+            {url: assetParentDirPath + "asset/texture/2.jpg", id: "texture2"}
         ]).subscribe(null, null, function () {
             initSample();
 
@@ -19,62 +20,29 @@ describe("generate correct image tool", function () {
         function initSample() {
             var director = wd.Director.getInstance();
 
-            director.scene.addChild(createSphere());
+            director.scene.addChild(createTriangle());
             director.scene.addChild(createCamera());
 
             //director.start();
         }
 
-        function createSphere() {
-            var canvas = mipmap(128, '#f00');
-            var textureCanvas = wd.ImageTexture.create(canvas);
-            textureCanvas.wrapS = wd.ETextureWrapMode.REPEAT;
-            textureCanvas.wrapT = wd.ETextureWrapMode.REPEAT;
-            textureCanvas.repeatRegion = wd.Vector4.create(0, 0, 10, 10);
-            textureCanvas.mipmaps.addChild(canvas);
-            textureCanvas.mipmaps.addChild(mipmap(64, '#0f0'));
-            textureCanvas.mipmaps.addChild(mipmap(32, '#00f'));
-            textureCanvas.mipmaps.addChild(mipmap(16, '#400'));
-            textureCanvas.mipmaps.addChild(mipmap(8, '#040'));
-            textureCanvas.mipmaps.addChild(mipmap(4, '#004'));
-            textureCanvas.mipmaps.addChild(mipmap(2, '#044'));
-            textureCanvas.mipmaps.addChild(mipmap(1, '#404'));
-            textureCanvas.needsUpdate = true;
-
-
+        function createTriangle() {
             var material = wd.BasicMaterial.create();
-            material.map = textureCanvas;
+            material.map = [wd.LoaderManager.getInstance().get("texture1").toTexture(), wd.LoaderManager.getInstance().get("texture2").toTexture()];
+            material.side = wd.ESide.BOTH;
 
 
-            var geometry = wd.SphereGeometry.create();
+            var geometry = wd.TriangleGeometry.create();
             geometry.material = material;
-            geometry.radius = 2;
-            geometry.segments = 30;
+            geometry.width = 5;
+            geometry.height = 5;
 
 
             var gameObject = wd.GameObject.create();
             gameObject.addComponent(geometry);
-
             gameObject.addComponent(wd.MeshRenderer.create());
 
-
             return gameObject;
-        }
-
-        function mipmap(size, color) {
-            var imageCanvas = document.createElement("canvas"),
-                context = imageCanvas.getContext("2d");
-
-            imageCanvas.width = imageCanvas.height = size;
-
-            context.fillStyle = "#444";
-            context.fillRect(0, 0, size, size);
-
-            context.fillStyle = color;
-            context.fillRect(0, 0, size / 2, size / 2);
-            context.fillRect(size / 2, size / 2, size / 2, size / 2);
-
-            return imageCanvas;
         }
 
         function createCamera() {
@@ -85,13 +53,12 @@ describe("generate correct image tool", function () {
             cameraComponent.fovy = 60;
             cameraComponent.aspect = view.width / view.height;
             cameraComponent.near = 0.1;
-            cameraComponent.far = 1000;
+            cameraComponent.far = 80;
 
-            var controller = wd.ArcballCameraController.create(cameraComponent);
-
-            controller.distance = 10;
-
+            var controller = wd.BasicCameraController.create(cameraComponent);
             camera.addComponent(controller);
+
+            camera.transform.translate(wd.Vector3.create(0, 0, 5));
 
             return camera;
         }
@@ -114,16 +81,7 @@ describe("generate correct image tool", function () {
             [
                 {
                     frameIndex:1,
-                    imageName:"texture_mipmap_manual_distance10.png"
-                },
-                {
-                    frameIndex:2,
-                    handle:function(){
-                        var camera = wd.Director.getInstance().scene.currentCamera;
-
-                        camera.getComponent(wd.CameraController).distance = 30;
-                    },
-                    imageName:"texture_mipmap_manual_distance30.png"
+                    imageName:"texture_multi.png"
                 }
             ]
         );
