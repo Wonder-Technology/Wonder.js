32e41d6b0821d43b161429745363e7c08bf442c1
yyc <395976266@qq.com>
2016-07-27 20:45:00 +0800

    render test: move camera_orthographic sample to render scene test

--- test/render/tool/generateCorrectImageToolSpec.js
+++ test/render/tool/generateCorrectImageToolSpec.js
@@ -4,73 +4,76 @@ describe("generate correct image tool", function () {
 
     function body(wrapper){
         wrapper.load([
-            {url: "../../asset/model/wd/male02/male02.wd", id: "model"}
             ])
             .do(initSample);
 
         function initSample() {
             var director = wd.Director.getInstance();
 
-            director.renderer.setClearColor(wd.Color.create("#aaaaff"));
-
-            director.scene.addChild(setModel());
-            director.scene.addChild(createAmbientLight());
-            director.scene.addChild(createDirectionLight());
+            director.scene.addChild(createTriangle1());
+            director.scene.addChild(createTriangle2());
             director.scene.addChild(createCamera());
 
             director.start();
         }
 
-        function setModel() {
-            var model = wd.LoaderManager.getInstance().get("model").getChild("models").getChild(0);
-
-            model.transform.translate(0, -80, 0);
-
-            model.getChildren()
-                .forEach(function (child) {
-                    var material = child.getComponent(wd.Geometry).material;
-                    material.shading = wd.EShading.SMOOTH;
-                });
+        function createTriangle1() {
+            var material = wd.BasicMaterial.create();
+            material.color= wd.Color.create("rgb(255, 0, 0)");
+            material.side = wd.ESide.BOTH;
 
 
-            return model;
+            var geometry = wd.TriangleGeometry.create();
+            geometry.material = material;
+            geometry.width = 20;
+            geometry.height = 20;
+
+
+            var gameObject = wd.GameObject.create();
+            gameObject.addComponent(geometry);
+
+            gameObject.addComponent(wd.MeshRenderer.create());
+
+            return gameObject;
         }
 
-        function createAmbientLight() {
-            var ambientLightComponent = wd.AmbientLight.create();
-            ambientLightComponent.color = wd.Color.create("rgb(30, 30, 30)");
 
-            var ambientLight = wd.GameObject.create();
-            ambientLight.addComponent(ambientLightComponent);
-
-            return ambientLight;
-        }
-
-        function createDirectionLight() {
-            var directionLightComponent = wd.DirectionLight.create();
-            directionLightComponent.color = wd.Color.create("#ffffff");
-            directionLightComponent.intensity = 2;
+        function createTriangle2() {
+            var material = wd.BasicMaterial.create();
+            material.color= wd.Color.create("rgb(255, 255, 0)");
+            material.side = wd.ESide.BOTH;
 
 
-            var directionLight = wd.GameObject.create();
-            directionLight.addComponent(directionLightComponent);
+            var geometry = wd.TriangleGeometry.create();
+            geometry.material = material;
+            geometry.width = 20;
+            geometry.height = 20;
 
 
-            return directionLight;
+            var gameObject = wd.GameObject.create();
+            gameObject.addComponent(geometry);
+
+            gameObject.addComponent(wd.MeshRenderer.create());
+
+            gameObject.transform.translate(0, 0, 30);
+
+            return gameObject;
         }
 
         function createCamera() {
             var camera = wd.GameObject.create(),
-                view = wd.Director.getInstance().view,
-                cameraComponent = wd.PerspectiveCamera.create();
+                cameraComponent = wd.OrthographicCamera.create();
 
-            cameraComponent.fovy = 60;
-            cameraComponent.aspect = view.width / view.height;
+            cameraComponent.left = -100;
+            cameraComponent.right = 100;
+            cameraComponent.top = 100;
+            cameraComponent.bottom = -100;
             cameraComponent.near = 0.1;
             cameraComponent.far = 1000;
 
             var controller = wd.ArcballCameraController.create(cameraComponent);
-            controller.distance = 200;
+            controller.distance = 50;
+            controller.phi = Math.PI / 2.5;
 
             camera.addComponent(controller);
 
@@ -100,19 +103,7 @@ describe("generate correct image tool", function () {
             [
                 {
                     frameIndex:1,
-                    imageName:"camera_arcball_control_viewfromFront.png"
-                },
-                {
-                    frameIndex:1,
-                    handle:function(){
-                        var camera = wd.Director.getInstance().scene.currentCamera;
-
-                        var controller = camera.getComponent(wd.CameraController);
-
-                        controller.phi = Math.PI / 8
-                        controller.theta = 0
-                    },
-                    imageName:"camera_arcball_control_viewfromTop.png"
+                    imageName:"camera_orthographic.png"
                 }
             ]
         );
