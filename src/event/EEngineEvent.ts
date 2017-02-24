e536a46357c20ac84e7e85f446b8af203803d805
yyc <395976266@qq.com>
2017-02-24 14:15:32 +0800

    refactor: remove not used engine event enum

--- src/event/EEngineEvent.ts
+++ src/event/EEngineEvent.ts
@@ -11,26 +11,26 @@ export enum EEngineEvent {
     POINT_SCALE = <any>"wd_pointscale",
     POINT_DRAG = <any>"wd_pointdrag",
 
-    MATERIAL_CHANGE = <any>"wd_material_change",
+    // MATERIAL_CHANGE = <any>"wd_material_change",
 
-    UI_WIDTH_CHANGE = <any>"wd_ui_width_change",
-    UI_HEIGHT_CHANGE = <any>"wd_ui_height_change",
+    // UI_WIDTH_CHANGE = <any>"wd_ui_width_change",
+    // UI_HEIGHT_CHANGE = <any>"wd_ui_height_change",
 
     TRANSFORM_TRANSLATE = <any>"wd_transform_translate",
     TRANSFORM_ROTATE = <any>"wd_transform_rotate",
-    TRANSFORM_SCALE = <any>"wd_transform_scale",
+    TRANSFORM_SCALE = <any>"wd_transform_scale"
 
-    SHADOWMAP_SOFTTYPE_CHANGE = <any>"wd_shadowMap_softType_change",
+    // SHADOWMAP_SOFTTYPE_CHANGE = <any>"wd_shadowMap_softType_change",
+    //
+    // SHADOWMAP_LAYER_CHANGE = <any>"wd_shadowMap_layer_change",
 
-    SHADOWMAP_LAYER_CHANGE = <any>"wd_shadowMap_layer_change",
+    // COMPONENT_CHANGE = <any>"wd_component_change",
 
-    COMPONENT_CHANGE = <any>"wd_component_change",
+    // AFTER_SCENEGRAPH_BUILD = <any>"wd_after_sceneGraph_build",
 
-    AFTER_SCENEGRAPH_BUILD = <any>"wd_after_sceneGraph_build",
-
-    ANIMATION_STOP = <any>"wd_animation_stop",
+    // ANIMATION_STOP = <any>"wd_animation_stop",
 
 
-    EXIT = <any>"wd_exit",
-    ENTER = <any>"wd_enter"
+    // EXIT = <any>"wd_exit",
+    // ENTER = <any>"wd_enter"
 }
