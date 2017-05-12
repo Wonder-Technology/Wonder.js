46c65be2bc0b8c14eed5a8f5eea10351fc4baf4d
yyc <395976266@qq.com>
2017-05-12 22:41:24 +0800

    optimize: delete obj val: now use "obj[key] = void 0" instead of using "delete obj[key]

--- src/utils/objectUtils.ts
+++ src/utils/objectUtils.ts
@@ -1,6 +1,6 @@
 import { isNotUndefined } from "./JudgeUtils";
 
-export var deleteVal = (key:string|number, obj:object) => delete obj[key];
+export var deleteVal = (key:string|number, obj:object) => obj[key] = void 0;
 
 export var isValidMapValue = (val:any) => {
     return isNotUndefined(val);
