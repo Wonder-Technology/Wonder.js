16bb77bbd4b9e72f617a522b83a53d3a1ecbae3c
yyc <395976266@qq.com>
2017-08-23 20:13:01 +0800

    feat: try pass ci

--- karma.conf.webgl1.noWorker.js
+++ karma.conf.webgl1.noWorker.js
@@ -76,7 +76,7 @@
 
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        browsers: ['Firefox'],
+        browsers: ['Chrome'],
 
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
