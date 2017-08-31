4ad5ee768bf3aa045e7e10e34ae600bf90d1b833
yyc <395976266@qq.com>
2017-08-31 16:22:53 +0800

    refactor: move contract.ts->describe,it,requireCheckFunc, ensureFunc to wonder-commonlib Matrix4.ts, TimeController.ts use "expect" instead of "assert"

--- src/definition/typescript/decorator/contract.ts
+++ src/definition/typescript/decorator/contract.ts
@@ -1,9 +1,16 @@
-import { Log } from "../../../utils/Log";
 import { CompileConfig } from "../../../config/CompileConfig";
 import { InitConfigData } from "../../../renderer/config/InitConfigData";
 import { InitConfigWorkerData } from "../../../renderer/worker/render_file/config/InitConfigWorkerData";
+import {
+    describe as describeCommonLib,
+    it as itCommonLib,
+    ensureFunc as ensureFuncCommonLib,
+    requireFunc as requireFuncCommonLib
+} from "wonder-commonlib/dist/es2015/typescript/decorator/contract";
 
 var _describeContext = null;
+
+var _getCompileIsTest = () => CompileConfig.isCompileTest;
 
 var _getIsTest = () => {
     if (InitConfigData.isTest === true || InitConfigWorkerData.isTest === true) {
@@ -13,48 +20,17 @@ var _getIsTest = () => {
     return false;
 }
 
-export function assert(cond: boolean, message: string = "contract error") {
-    Log.error(!cond, message);
-}
+// export function assert(cond: boolean, message: string = "contract error") {
+//     Log.error(!cond, message);
+// }
+//
+export var describe = describeCommonLib;
 
-export function describe(message: string, func: Function, preCondition: Function = () => true, context: any = this) {
-    if (preCondition.call(context, null)) {
-        _describeContext = context;
-
-        try {
-            func.call(context, null);
-        }
-        catch (e) {
-            assert(false, `${message}->${e.message}`);
-        }
-        finally {
-            _describeContext = null;
-        }
-    }
-}
-
-export function it(message: string, func: Function, context?: any) {
-    try {
-        if (arguments.length === 3) {
-            func.call(context, null);
-        }
-        else {
-            if (_describeContext) {
-                func.call(_describeContext, null);
-            }
-            else {
-                func();
-            }
-        }
-    }
-    catch (e) {
-        assert(false, `${message}->${e.message}`);
-    }
-}
+export var it = itCommonLib;
 
 export function requireCheck(inFunc) {
     return function(target, name, descriptor) {
-        if (CompileConfig.isCompileTest) {
+        if (_getCompileIsTest()) {
             let value = descriptor.value;
 
             descriptor.value = function(args) {
@@ -71,19 +47,7 @@ export function requireCheck(inFunc) {
 }
 
 export function requireCheckFunc(checkFunc: Function, bodyFunc: Function) {
-    if (!CompileConfig.isCompileTest) {
-        return bodyFunc;
-    }
-
-    return (...paramArr) => {
-        if (!_getIsTest()) {
-            return bodyFunc.apply(null, paramArr);
-        }
-
-        checkFunc.apply(null, paramArr);
-
-        return bodyFunc.apply(null, paramArr);
-    }
+    return requireFuncCommonLib(checkFunc, bodyFunc, _getCompileIsTest(), _getIsTest);
 }
 
 export function ensure(outFunc) {
@@ -114,117 +78,103 @@ export function ensure(outFunc) {
 }
 
 export function ensureFunc(checkFunc: Function, bodyFunc: Function) {
-    if (!CompileConfig.isCompileTest) {
-        return bodyFunc;
-    }
-
-    return (...paramArr) => {
-        if (!_getIsTest()) {
-            return bodyFunc.apply(null, paramArr);
-        }
-
-        let result = bodyFunc.apply(null, paramArr);
-
-        checkFunc.apply(null, [result].concat(paramArr));
-
-        return result;
-    }
+    return ensureFuncCommonLib(checkFunc, bodyFunc, _getCompileIsTest(), _getIsTest);
 }
-
-export function requireGetterAndSetter(inGetterFunc, inSetterFunc) {
-    return function(target, name, descriptor) {
-        if (CompileConfig.isCompileTest) {
-            let getter = descriptor.get,
-                setter = descriptor.set;
-
-            descriptor.get = function() {
-                if (_getIsTest()) {
-                    inGetterFunc.call(this);
-                }
-
-                return getter.call(this);
-            };
-
-            descriptor.set = function(val) {
-                if (_getIsTest()) {
-                    inSetterFunc.call(this, val);
-                }
-
-                setter.call(this, val);
-            };
-        }
-
-        return descriptor;
-    }
-}
-
-export function requireGetter(inFunc) {
-    return function(target, name, descriptor) {
-        if (CompileConfig.isCompileTest) {
-            let getter = descriptor.get;
-
-            descriptor.get = function() {
-                if (_getIsTest()) {
-                    inFunc.call(this);
-                }
-
-                return getter.call(this);
-            };
-
-        }
-
-        return descriptor;
-    }
-}
-
-export function requireSetter(inFunc) {
-    return function(target, name, descriptor) {
-        if (CompileConfig.isCompileTest) {
-            let setter = descriptor.set;
-
-            descriptor.set = function(val) {
-                if (_getIsTest()) {
-                    inFunc.call(this, val);
-                }
-
-                setter.call(this, val);
-            };
-        }
-
-        return descriptor;
-    }
-}
-
-export function ensureGetterAndSetter(outGetterFunc, outSetterFunc) {
-    return function(target, name, descriptor) {
-        if (CompileConfig.isCompileTest) {
-            let getter = descriptor.get,
-                setter = descriptor.set;
-
-            descriptor.get = function() {
-                var result = getter.call(this);
-
-                if (_getIsTest()) {
-                    outGetterFunc.call(this, result);
-                }
-
-                return result;
-            };
-
-            descriptor.set = function(val) {
-                var result = setter.call(this, val);
-
-                if (_getIsTest()) {
-                    let params = [result, val];
-                    outSetterFunc.apply(this, params);
-                }
-            };
-        }
-
-        return descriptor;
-    }
-}
-
+//
+// export function requireGetterAndSetter(inGetterFunc, inSetterFunc) {
+//     return function(target, name, descriptor) {
+//         if (CompileConfig.isCompileTest) {
+//             let getter = descriptor.get,
+//                 setter = descriptor.set;
+//
+//             descriptor.get = function() {
+//                 if (_getIsTest()) {
+//                     inGetterFunc.call(this);
+//                 }
+//
+//                 return getter.call(this);
+//             };
+//
+//             descriptor.set = function(val) {
+//                 if (_getIsTest()) {
+//                     inSetterFunc.call(this, val);
+//                 }
+//
+//                 setter.call(this, val);
+//             };
+//         }
+//
+//         return descriptor;
+//     }
+// }
+//
+// export function requireGetter(inFunc) {
+//     return function(target, name, descriptor) {
+//         if (CompileConfig.isCompileTest) {
+//             let getter = descriptor.get;
+//
+//             descriptor.get = function() {
+//                 if (_getIsTest()) {
+//                     inFunc.call(this);
+//                 }
+//
+//                 return getter.call(this);
+//             };
+//
+//         }
+//
+//         return descriptor;
+//     }
+// }
+//
+// export function requireSetter(inFunc) {
+//     return function(target, name, descriptor) {
+//         if (CompileConfig.isCompileTest) {
+//             let setter = descriptor.set;
+//
+//             descriptor.set = function(val) {
+//                 if (_getIsTest()) {
+//                     inFunc.call(this, val);
+//                 }
+//
+//                 setter.call(this, val);
+//             };
+//         }
+//
+//         return descriptor;
+//     }
+// }
+//
+// export function ensureGetterAndSetter(outGetterFunc, outSetterFunc) {
+//     return function(target, name, descriptor) {
+//         if (CompileConfig.isCompileTest) {
+//             let getter = descriptor.get,
+//                 setter = descriptor.set;
+//
+//             descriptor.get = function() {
+//                 var result = getter.call(this);
+//
+//                 if (_getIsTest()) {
+//                     outGetterFunc.call(this, result);
+//                 }
+//
+//                 return result;
+//             };
+//
+//             descriptor.set = function(val) {
+//                 var result = setter.call(this, val);
+//
+//                 if (_getIsTest()) {
+//                     let params = [result, val];
+//                     outSetterFunc.apply(this, params);
+//                 }
+//             };
+//         }
+//
+//         return descriptor;
+//     }
+// }
+//
 export function ensureGetter(outFunc) {
     return function(target, name, descriptor) {
         if (CompileConfig.isCompileTest) {
@@ -245,31 +195,31 @@ export function ensureGetter(outFunc) {
     }
 }
 
-export function ensureSetter(outFunc) {
-    return function(target, name, descriptor) {
-        if (CompileConfig.isCompileTest) {
-            let setter = descriptor.set;
+// export function ensureSetter(outFunc) {
+//     return function(target, name, descriptor) {
+//         if (CompileConfig.isCompileTest) {
+//             let setter = descriptor.set;
+//
+//             descriptor.set = function(val) {
+//                 var result = setter.call(this, val);
+//
+//                 if (_getIsTest()) {
+//                     let params = [result, val];
+//                     outFunc.apply(this, params);
+//                 }
+//             };
+//         }
+//
+//         return descriptor;
+//     }
+// }
 
-            descriptor.set = function(val) {
-                var result = setter.call(this, val);
-
-                if (_getIsTest()) {
-                    let params = [result, val];
-                    outFunc.apply(this, params);
-                }
-            };
-        }
-
-        return descriptor;
-    }
-}
-
-export function invariant(func) {
-    return function(target) {
-        if (CompileConfig.isCompileTest) {
-            if (_getIsTest()) {
-                func(target);
-            }
-        }
-    }
-}
+// export function invariant(func) {
+//     return function(target) {
+//         if (CompileConfig.isCompileTest) {
+//             if (_getIsTest()) {
+//                 func(target);
+//             }
+//         }
+//     }
+// }
