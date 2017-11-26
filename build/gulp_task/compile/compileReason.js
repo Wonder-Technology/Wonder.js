50e61df58bcc04615045a73c016f6d39d41b15e9
yyc <395976266@qq.com>
2017-11-26 16:48:42 +0800

    chore: package.json: upgrade wonder-glsl-compiler
    fix: fix gulp task->compileReason
    description
    err message: stdout maxBuffer exceeded.
    
    reason
    refer to https://stackoverflow.com/questions/23429499/stdout-buffer-issue-using-node-child-process :
    maxBuffer specifies the largest amount of data allowed on stdout or stderr - if this value is exceeded then the child process is killed.
    The documentation also states that the default value of maxBuffer is 200KB.
    
    solution
    the maximum buffer size is increased to 500KB in the following code:
    gulp.task("compileReason", function (done) {
        exec("npm run build", { maxBuffer: 1024 * 500 }, function (err, stdout, stderr) {
            if (err) {
                throw err;
            }
    
            done();
        });
    });

--- build/gulp_task/compile/compileReason.js
+++ build/gulp_task/compile/compileReason.js
@@ -2,8 +2,8 @@ var gulp = require("gulp");
 var exec = require("child_process").exec;
 
 gulp.task("compileReason", function (done) {
-    exec("npm run build", function (err, stdout, stderr) {
-        if(err){
+    exec("npm run build", { maxBuffer: 1024 * 500 }, function (err, stdout, stderr) {
+        if (err) {
             throw err;
         }
 
