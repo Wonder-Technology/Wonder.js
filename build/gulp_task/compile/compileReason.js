6cb4f306789d2fd2eec2a1a6a14c18e6b433c1c9
yyc <395976266@qq.com>
2017-12-24 18:31:43 +0800

    fix: compileReason gulp task: add "sudo"

--- build/gulp_task/compile/compileReason.js
+++ build/gulp_task/compile/compileReason.js
@@ -2,7 +2,7 @@ var gulp = require("gulp");
 var exec = require("child_process").exec;
 
 gulp.task("compileReason", function (done) {
-    exec("npm run build", { maxBuffer: 1024 * 500 }, function (err, stdout, stderr) {
+    exec("sudo npm run build", { maxBuffer: 1024 * 500 }, function (err, stdout, stderr) {
         if (err) {
             throw err;
         }
