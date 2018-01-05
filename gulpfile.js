2eb6b98366d88831f47613a750053a9c74fc0664
yyc <395976266@qq.com>
2018-01-05 16:57:45 +0800

    test(pf-test): fix gulpfile.js->require

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var gulpSync = require("gulp-sync")(gulp);
 
 require("./build/gulp_task/create_inner_file/shaderChunk");
 require("./build/gulp_task/rollup/rollup");
-require("./build/gulp_task/performance/generateData");
+require("./build/gulp_task/performance/testPerformance");
 require("./build/gulp_task/render/testRender");
 
 
