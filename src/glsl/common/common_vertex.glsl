2db35707aa33b107b77de537e4e8e5c7659975ff
yyc <395976266@qq.com>
2018-05-16 17:20:56 +0800

    refactor(dead-code): remove common_vertex.glsl dead code

--- src/glsl/common/common_vertex.glsl
+++ src/glsl/common/common_vertex.glsl
@@ -1,24 +1,7 @@
 @define
 #import "common_define"
-
-// #export 
-
-// ##name
-// aaaa
-// ##end
-
-// ##body
-
-
-// ##end
-
-
-// #end
-    
-
 @end
 
 @funcDefine
-// #import * from "common_function"
 #import "common_function"
 @end
